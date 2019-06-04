cc5f595c6b85294e1285426107f8f4f6466a802a
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-03 18:17:52 -0700

    test_1file.c test_multiple_files.c test_stdin.c

---
+++ test_1file.c
@@ -0,0 +1,27 @@
+#include "get_next_line.h"
+#include <stdio.h>
+
+int		main(void)
+{
+	char	*line = NULL;
+	int		fd;
+
+	if ((fd = open("./input1.txt", O_RDONLY)) == 1)
+		printf("file open error\n");
+	else
+		printf("fd of input1.txt is:%d\n", fd);
+
+	printf("<<<<<<<<<<<<<<<<<<<< fd%d\n", fd);
+	while (get_next_line(fd, &line))
+	{
+		ft_putendl(line);
+		ft_strdel(&line);
+	}
+	printf(">>>>>>>>>>>>>>>>>>>> fd%d\n", fd);
+	
+	close(fd);
+
+	// while (1); // keep process running
+
+	return (0);
+}
