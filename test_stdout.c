0e5e4375a523dcc30d9029a6901cd601cad9676a
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-04 20:28:39 -0700

    renamed file

---
+++ test_stdout.c
@@ -0,0 +1,21 @@
+#include "get_next_line.h"
+
+int		main(void)
+{
+	char	*line = NULL;
+
+	while (get_next_line(1, &line))
+	{
+		ft_putendl(line);
+		ft_strdel(&line);
+	}
+
+	return (0);
+}
+
+/*
+ * execute binary
+ * (program waits for input)
+ * type some characters, hit return to separate lines
+ * ^D to end input
+ */
