90a7a4c24b80924889bf144353c1ee61c1566d36
Eisuke Esaki <eesaki@student.42.us.org>
2019-11-20 17:01:15 -0800

    added test for reading blank files

---
+++ test_blankfile.c
@@ -0,0 +1,28 @@
+#include "../get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int		main(void)
+{
+	char	*line = NULL;
+	int		fd;
+
+	if ((fd = open("tests/input_blank.txt", O_RDONLY)) == 1)
+		printf("file open error\n");
+	else
+		printf("fd of input1.txt is:%d\n", fd);
+
+	printf("<<<<<<<<<<<<<<<<<<<< fd%d\n", fd);
+	while (get_next_line(fd, &line))
+	{
+		ft_putendl(line);
+		ft_strdel(&line);
+	}
+	printf(">>>>>>>>>>>>>>>>>>>> fd%d\n", fd);
+	
+	close(fd);
+
+	// while (1); // keep process running
+
+	return (0);
+}
